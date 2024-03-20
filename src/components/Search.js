39a0d8bd86cc3ff463a401bd00263f5899e20237
itsajayk <itsajayk@outlook.com>
2024-03-20 16:24:15 +0530

    Updated Project

---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+function SearchBar(){
+        return(
+        <div className="searchbar">
+            <input type="text" placeholder="Search"/>
+        </div>
+        )
+    }
+
+export default SearchBar
